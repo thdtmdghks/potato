dc7e1a7cd6370ff3e95d74afa0e2deffcf849814
thdtmdghks <ghksl4157@gmail.com>
2026-05-20 15:26:22 +0900

    chore: commitlint에 merge type 추가

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,25 @@
-const config = { extends: ["@commitlint/config-conventional"] };
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "merge",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
 
 export default config;
